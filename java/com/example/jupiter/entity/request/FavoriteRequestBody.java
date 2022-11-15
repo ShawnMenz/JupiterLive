38209eae4f541e825423b8ead2e4e4b4ea6f7e86
Zexuan Meng <mr.zexuan.meng@gmail.com>
2022-11-15 00:51:29 -0500

    init

---
+++ java/com/example/jupiter/entity/request/FavoriteRequestBody.java
@@ -0,0 +1,18 @@
+package com.example.jupiter.entity.request;
+
+import com.example.jupiter.entity.db.Item;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class FavoriteRequestBody {
+    private final Item favoriteItem;
+
+    @JsonCreator
+    public FavoriteRequestBody(@JsonProperty("favorite") Item favoriteItem) {
+        this.favoriteItem = favoriteItem;
+    }
+
+    public Item getFavoriteItem() {
+        return favoriteItem;
+    }
+}
